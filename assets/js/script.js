dcbe00073e660b94a9900532583d0cbb9d9f182f
Yurek84 <ilnytskyj84@gmail.com>
2020-07-01 22:55:07 +0300

    Ínit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+
+//Задача 10
+var arr = ['1', '2', '3'];
+var newLength = arr.unshift('4', '5', '6');
+console.log(arr);
+
+//Задача 11
+var arr = ['js', 'css', 'jq'];
+console.log(arr[arr.length-1])
+
+
+//Задача 12
+var arr = ['1','2','3','4','5'];
+console.log(arr.slice(0,3));
+
+
+//Задача 13
+var arr = ['1','2','3','4','5'];
+arr.splice(2,2, '10', '11');
+console.log(arr);
